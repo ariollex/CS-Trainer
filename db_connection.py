abc5bfe2025af856d1c1f2d75b6d93359b9cb8dc
las-tochka <takuve@yandex.ru>
2025-04-16 10:56:08 +0300

    db_connection: get_fundamentals_sort, get_algorithms_sort, fund_alg_from_data_to_dct information format from_data_to_dct->users_from_data_to_dct

--- db_connection.py
+++ db_connection.py
@@ -6,12 +6,12 @@ def get_dict_users():
     query = f"SELECT * FROM users;"  # Запрос для получения всех данных из таблицы
     cursor.execute(query)
     data = cursor.fetchall()
-    records = from_data_to_dct(data)
+    records = users_from_data_to_dct(data)
     return records
 
 def save_user(email, password, nickname, verified, verification_code):
     try:
-        query = f"""insert users(email, password, nickname, achievenment, avatar, fundamentals, algorithms, verified, verification_code)
+        query = f"""insert users(email, password, nickname, achievement, avatar, fundamentals, algorithms, verified, verification_code)
             values ('{email}', '{password}', '{nickname}', '123', 'aaa', 0, 0, {verified}, '{verification_code}')"""
         cursor.execute(query)
         connection.commit()
@@ -23,23 +23,23 @@ def save_user(email, password, nickname, verified, verification_code):
 def get_fundamentals_sort():
     query = f"""SELECT fundamentals.*, users.nickname, users.achievement, users.avatar
                 FROM fundamentals
-                JOIN users ON fundamentals.user_id = users.id
-                ORDER BY fundamentals.score;"""
+                JOIN users ON fundamentals.users_id = users.id
+                ORDER BY fundamentals.score"""
     cursor.execute(query)
     data = cursor.fetchall()
-    # records = from_data_to_dct(data)
-    # return records
+    records = fund_alg_from_data_to_dct(data)
+    return records
 
 
 def get_algorithms_sort():
     query = f"""SELECT algorithms.*, users.nickname, users.achievement, users.avatar
                 FROM algorithms
-                JOIN users ON algorithms.user_id = users.id
+                JOIN users ON algorithms.users_id = users.id
                 ORDER BY algorithms.score;"""
     cursor.execute(query)
     data = cursor.fetchall()
-    # records = from_data_to_dct(data)
-    # return records
+    records = fund_alg_from_data_to_dct(data)
+    return records
 
 
 
@@ -63,9 +63,9 @@ def user_information(email):
         return 'not_found'
     if len(data) > 1:
         return 'found_more'
-    return from_data_to_dct(data)
+    return users_from_data_to_dct(data)
 
-def from_data_to_dct(data):
+def users_from_data_to_dct(data):
     records = dict()
     for i in data:
         records[i[1]] = {'id': i[0],
@@ -80,6 +80,20 @@ def from_data_to_dct(data):
                'verification_code': i[9]}
     return records
 
+def fund_alg_from_data_to_dct(data):
+    records = dict()
+    for i in data:
+        records[i[1]] = {'id': i[0],
+                'users_id': i[1],
+               'score': i[2],
+               'testPassed': i[3],
+               'totalTests': i[4],
+               'lastActivity': i[5],
+               'nickname': i[6],
+               'achievement': i[7]
+            }
+    return records
+
 with open('database_user_home.json') as file:
     file_json_data = json.load(file)
 try:
@@ -90,6 +104,7 @@ try:
         database=file_json_data['database']
     )
     cursor = connection.cursor()
+    print(get_algorithms_sort())
 except pymysql.MySQLError as e:
     print(f"Ошибка подключения: {e}")
 # finally:
